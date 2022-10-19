90c7eb2cfb30ceea1c1555d8ca2996945bc64b9b
marcoslacoa <marcos.lacoa@davinci.edu.ar>
2022-10-19 01:00:21 -0300

    cambios

--- TrabajoPlataformas/CrearCaja.cs
+++ TrabajoPlataformas/CrearCaja.cs
@@ -22,6 +22,8 @@ namespace TrabajoPlataformas
             InitializeComponent();
             this.banco = banco;
             this.usuario = usuario;
+            
+            this.cbuCaja = usuario.dni + new Random().Next(10000, 99999);
         }
 
         private void cbu_TextChanged(object sender, EventArgs e)
@@ -31,13 +33,36 @@ namespace TrabajoPlataformas
 
         private void confirmar_Click(object sender, EventArgs e)
         {
-            cbuCaja = int.Parse(cbu.Text);
             saldoCaja = float.Parse(saldo.Text);
             banco.crearCajaAhorro(cbuCaja, saldoCaja);
             this.Close();
         }
 
         private void CrearCaja_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saldo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void cbu_load_1(object sender, EventArgs e)
+        {
+
+            // show this.cbuCaja and make it readonly
+            cbu.Text = cbuCaja.ToString();
+            cbu.ReadOnly = true;
+
+
+        }
+        private void saldo_load_1(object sender, EventArgs e)
+        {
+            saldo.Text = "0";
+            saldo.ReadOnly = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
