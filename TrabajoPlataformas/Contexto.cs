9171075e6cbd9b08559a3226a65543d00ca3e443
marcoslacoa <marcos.lacoa@davinci.edu.ar>
2022-11-29 01:29:36 -0300

    a

--- TrabajoPlataformas/Contexto.cs
+++ TrabajoPlataformas/Contexto.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace TrabajoPlataformas
 {
@@ -67,25 +68,29 @@ namespace TrabajoPlataformas
             .HasOne(D => D.titular)
             .WithMany(U => U.plazoFijo)
             .HasForeignKey(D => D.id)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(false);
 
             modelBuilder.Entity<Pago>()
             .HasOne(D => D.usuario)
             .WithMany(U => U.pagos)
             .HasForeignKey(D => D.id)
-            .OnDelete(DeleteBehavior.Cascade);
-
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(false);
+       
             modelBuilder.Entity<Movimiento>()
             .HasOne(D => D.caja)
             .WithMany(U => U.movimientos)
             .HasForeignKey(D => D.id)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(false);
 
             modelBuilder.Entity<TarjetaCredito>()
             .HasOne(D => D.titular)
             .WithMany(U => U.tarjetas)
             .HasForeignKey(D => D.id)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(false);
 
 
             //RELACIONES MANY TO MANY
@@ -101,14 +106,21 @@ namespace TrabajoPlataformas
 
             //propiedades de los datos
             modelBuilder.Entity<Usuario>(
-                usr =>
-                {
+            usr =>
+            {
+                    usr.Property(u => u.id).HasColumnType("int");
+                    usr.Property(u => u.id).IsRequired(true);
                     usr.Property(u => u.nombre).HasColumnType("varchar(50)");
-                    usr.Property(u => u.apellido).HasColumnType("varchar(50)");
-                    usr.Property(u => u.dni).HasColumnType("int(11)");
+                     usr.Property(u => u.nombre).IsRequired(true);
+                     usr.Property(u => u.apellido).HasColumnType("varchar(50)");
+                    usr.Property(u => u.apellido).IsRequired(true);
+                    usr.Property(u => u.dni).HasColumnType("int");
+                    usr.Property(u => u.dni).IsRequired(true);
                     usr.Property(u => u.mail).HasColumnType("varchar(512)");
+                    usr.Property(u => u.mail).IsRequired(true);
                     usr.Property(u => u.contra).HasColumnType("varchar(50)");
-                    usr.Property(u => u.intentosFallidos).HasColumnType("int(11)");
+                    usr.Property(u => u.contra).IsRequired(true);
+                    usr.Property(u => u.intentosFallidos).HasColumnType("int");
                     usr.Property(u => u.esADM).HasColumnType("bit");
                     usr.Property(u => u.bloqueado).HasColumnType("bit");
                 });
@@ -116,47 +128,74 @@ namespace TrabajoPlataformas
             modelBuilder.Entity<CajaAhorro>(
                 usr =>
                 {
-                    usr.Property(u => u.cbu).HasColumnType("int(11)");
+                    usr.Property(u => u.id).HasColumnType("int");
+                    usr.Property(u => u.id).IsRequired(true);
+                    usr.Property(u => u.cbu).HasColumnType("int");
+                    usr.Property(u => u.cbu).IsRequired(true);
                     usr.Property(u => u.saldo).HasColumnType("float");
+                    usr.Property(u => u.saldo).IsRequired(true);
                 });
 
             modelBuilder.Entity<TarjetaCredito>(
                 usr =>
                 {
-                    //usr.Property(u => u.titular).HasColumnType("int(11)");
-                    usr.Property(u => u.numero).HasColumnType("int(11)");
+                    //usr.Property(u => u.titular).HasColumnType("int");
+                    usr.Property(u => u.id).HasColumnType("int");
+                    usr.Property(u => u.id).IsRequired(true);
+                    usr.Property(u => u.numero).HasColumnType("int");
+                    usr.Property(u => u.numero).IsRequired(true);
                     usr.Property(u => u.consumos).HasColumnType("float");
-                    usr.Property(u => u.limite).HasColumnType("int(11)");
-                    usr.Property(u => u.codigoSeguridad).HasColumnType("int(11)");
+                    usr.Property(u => u.consumos).IsRequired(true);
+                    usr.Property(u => u.limite).HasColumnType("int");
+                    usr.Property(u => u.limite).IsRequired(true);
+                    usr.Property(u => u.codigoSeguridad).HasColumnType("int");
+                    usr.Property(u => u.codigoSeguridad).IsRequired(true);
                 });
 
             modelBuilder.Entity<Pago>(
                 usr =>
                 {
+                    usr.Property(u => u.id).HasColumnType("int");
+                    usr.Property(u => u.id).IsRequired(true);
                     usr.Property(u => u.detalle).HasColumnType("varchar(50)");
+                    usr.Property(u => u.detalle).IsRequired(true);
                     //usr.Property(u => u.usuario).HasColumnType("varchar(50)");
                     usr.Property(u => u.monto).HasColumnType("float");
+                    usr.Property(u => u.monto).IsRequired(true);
                     usr.Property(u => u.pagado).HasColumnType("bit");
+                    usr.Property(u => u.pagado).IsRequired(true);
                 });
 
             modelBuilder.Entity<PlazoFijo>(
                usr =>
                {
                    //usr.Property(u => u.titular).HasColumnType("varchar(50)");
-                   usr.Property(u => u.tasa).HasColumnType("int(11)");
+                   usr.Property(u => u.id).HasColumnType("int");
+                   usr.Property(u => u.id).IsRequired(true);
+                   usr.Property(u => u.tasa).HasColumnType("int");
+                   usr.Property(u => u.tasa).IsRequired(true);
                    usr.Property(u => u.fechaIni).HasColumnType("datetime");
+                   usr.Property(u => u.fechaIni).IsRequired(true);
                    usr.Property(u => u.FechaFin).HasColumnType("datetime");
+                   usr.Property(u => u.FechaFin).IsRequired(true);
                    usr.Property(u => u.pagado).HasColumnType("bit");
+                   usr.Property(u => u.pagado).IsRequired(true);
                    usr.Property(u => u.monto).HasColumnType("float");
+                   usr.Property(u => u.monto).IsRequired(true);
                });
 
             modelBuilder.Entity<Movimiento>(
                usr =>
                {
                    //usr.Property(u => u.caja).HasColumnType("varchar(50)");
+                   usr.Property(u => u.id).HasColumnType("int");
+                   usr.Property(u => u.id).IsRequired(true);
                    usr.Property(u => u.detalle).HasColumnType("varchar(50)");
+                   usr.Property(u => u.detalle).IsRequired(true);
                    usr.Property(u => u.monto).HasColumnType("float");
+                   usr.Property(u => u.monto).IsRequired(true);
                    usr.Property(u => u.fecha).HasColumnType("datetime");
+                   usr.Property(u => u.fecha).IsRequired(true);
                });
 
             // EXCLUIR
